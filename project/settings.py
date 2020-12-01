f7c844752cb1d74b4cc130a38c795ad8c5837d18
ayoub boughzala <ayoubboughzalaprep23@gmail.com>
2020-12-01 01:40:30 +0100

    first commit

--- project/settings.py
+++ project/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import dj_database_url
 import os
 import django_heroku
+import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
